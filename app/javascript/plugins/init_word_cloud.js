c2c8abc6ab346efc07f3e2d79cedf332870d3431
Oyvind Fossum <fooy1@pm.me>
2021-11-30 22:15:51 +0100

    minor updates word-cloud

--- app/javascript/plugins/init_word_cloud.js
+++ app/javascript/plugins/init_word_cloud.js
@@ -4,8 +4,11 @@ const initWordCloud = () => {
   const canvas = document.getElementById('my_canvas');
   if (canvas) {
     const list = JSON.parse(canvas.dataset.words)
-    WordCloud(canvas, { list: list, fontWeight: '600', weightFactor: '20', color: 'random-dark', rotationSteps: '2' });
+    WordCloud(canvas, {
+      list: list, fontFamily: 'Montserrat, monospace', fontWeight: '600', weightFactor: '7', color: 'black', rotationSteps: '2' });
   }
 }
 
 export { initWordCloud };
+
+// '#000000 #404040 #7f7f7f #bfbfbf'
