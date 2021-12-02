dcbd0d4c0e79d672ebfcbbdf0ca9dfad0fa716b6
javiersuay <jota.suay@gmail.com>
2021-12-02 14:25:09 +0100

    style in map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -43,8 +43,8 @@ const addMarkersToMap = (map, markers) => {
       const canvas = popupElement.querySelector('.word-cloud-canvas')
       initWordCloud(canvas)
       const pop = document.querySelector('.mapboxgl-popup.mapboxgl-popup-anchor-top')
-      pop.style.maxWidth = "900px";
-      pop.style.height = "300px";
+      // pop.style.maxWidth = "900px";
+      // pop.style.height = "300px";
       pop.style.fontFamily = "Inconsolata, monospace";
 
       // const pubOne =
@@ -58,7 +58,6 @@ const addMarkersToMap = (map, markers) => {
       const optOne = document.getElementById('comparison_publisher_one');
       const optTwo = document.getElementById('comparison_publisher_two')
       const showBlock = document.querySelector('.javi-block')
-      console.log(showBlock);
 
       btnAdd.addEventListener("click", (event) => {
         event.preventDefault()
@@ -67,23 +66,31 @@ const addMarkersToMap = (map, markers) => {
 
         if (optOne.value == "") {
           optOne.value = sourceName
-          optOne.style.display = "block";
-          optOne.style.zIndex = "10";
-          // sshowBlock.style.opacity = "1";
-
+          set_source(optOne)
+          // optOne.style.zIndex = "10";
+          // optOne.style.backgroundColor = "white";
+          // optOne.style.maxWidth = "200px";
+          // optOne.style.borderRadius = "18px";
+          // optOne.style.textAlign = "center";
 
         } else if (optOne.value != "" && optTwo.value == "") {
           optTwo.value = sourceName
-          optTwo.style.display = "block";
-          optTwo.style.zIndex = "10";
-          // showBlock.style.opacity = "1";
-
-
+          set_source(optTwo)
+          // optTwo.style.display = "block";
+          // optTwo.style.zIndex = "10";
+          // optTwo.style.backgroundColor = "white";
+          // // showBlock.style.opacity = "1";
         }
 
       });
-
-
+      const set_source = (element) =>{
+        element.style.position = "relative";
+        element.style.display = "block";
+        element.style.zIndex = "10";
+        element.style.backgroundColor = "white";
+        element.style.borderRadius = "18px";
+        element.style.textAlign = "center";
+      }
     });
 
     const element = document.createElement('div');
