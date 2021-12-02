441316e08891964f579166e1b4feaf8bfffa1cc0
Maximilian Steuck <max@fount.io>
2021-12-02 11:17:16 +0100

    Updated the popups a bit

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -45,6 +45,7 @@ const addMarkersToMap = (map, markers) => {
       initWordCloud(canvas)
       const pop = document.querySelector('.mapboxgl-popup.mapboxgl-popup-anchor-top')
       pop.style.maxWidth = "900px";
+      pop.style.height = "300px";
       pop.style.fontFamily = "Inconsolata, monospace";
 
       // const pubOne =
